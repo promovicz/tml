5ff038c4d8950bf250b17fa9c4189d09ff7aaf31
Ingo Albrecht <commit@promovicz.org>
2020-10-25 01:00:58 +0200

    Improve compile.sh

--- compile.sh
+++ compile.sh
@@ -5,6 +5,10 @@ set -e
 CFLAGS=
 LINK=
 
+if [ -z "${CC}" ]; then
+  CC=gcc
+fi
+
 # get us some color
 normal="$(tput sgr0)"
 red="$(tput setaf 1)"
@@ -46,9 +50,10 @@ if ! pkg expat; then
   fail "Need expat installed."
 fi
 
-if pkg tinfo; then
-  true
-elif pkg ncursesw; then
+# NOTE would like to use tinfo, but that fails on some systems
+#if pkg tinfo; then
+#  true
+if pkg ncursesw; then
   true
 elif pkg ncurses; then
   true
@@ -59,7 +64,7 @@ fi
 
 echo ""
 
-verbose gcc -Wall -Wextra -Wno-unused -g -O0 $CFLAGS $LINK -o tml2tty tml2tty.c
+verbose ${CC} -Wall -Wextra -Wno-unused -Wno-unused-parameter -g -O0 $CFLAGS $LINK -o tml2tty tml2tty.c
 
 echo ""
 
